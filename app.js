6d3b3912c66ec87fdcc482592490457b26fb63db
Sarvesh <142646866+sarvesh-official@users.noreply.github.com>
2023-10-04 14:58:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Selecting Players
 virender = document.getElementById('virender');
-sachin = document.getElementById('sachin');
+sachin = document.getElementById('sachin1');
 gautam = document.getElementById('gautam');
 virat = document.getElementById('virat');
 dhoni = document.getElementById('dhoni');
@@ -12,34 +12,128 @@ munaf = document.getElementById('munaf');
 sreesanth = document.getElementById('sreesanth');
 
 //Selecting Pictures
-virenderimg = document.querySelector('.virenderimg');
-sachinimg = document.querySelector('.sachinimg');
-gautamimg = document.querySelector('.gambhirimg');
-viratimg = document.querySelector('.viratimg');
-dhoniimg = document.querySelector('.dhoniimg');
-yuvrajimg = document.querySelector('.yuvrajimg');
-sureshimg = document.querySelector('.sureshimg');
-harbhajanimg = document.querySelector('.harbhajanimg');
-zaheerimg = document.querySelector('.zaheerimg');
-munafimg = document.querySelector('.munalimg');
-sreesanthimg = document.querySelector('.sreesanthimg');
+virenderimg = document.querySelector('#virenderimg');
+sachinimg = document.querySelector('#sachinimg');
+gautamimg = document.querySelector('#gambhirimg');
+viratimg = document.querySelector('#viratimg');
+dhoniimg = document.querySelector('#dhoniimg');
+yuvrajimg = document.querySelector('#yuvrajimg');
+sureshimg = document.querySelector('#sureshimg');
+harbhajanimg = document.querySelector('#harbhajanimg');
+zaheerimg = document.querySelector('#zaheerimg');
+munafimg = document.querySelector('#munafimg');
+sreesanthimg = document.querySelector('#sreesanthimg');
 
 // Adding Onclick Function
 
-function display(playername) {
-  playername.style.marginTop = '-50px';
-  playername.style.position = 'absolute';
-  playername.style.display = 'block';
+function showimage(){
+  virenderimg.style.display='block';
 }
+function hideimage() {
+  virenderimg.style.display='none';
+}
+virender.addEventListener('click',showimage)
 
-virender.addEventListener('click', display(virenderimg));
-sachin.addEventListener('click', display(sachinimg));
-gautam.addEventListener('click', display(gautamimg));
-virat.addEventListener('click', display(viratimg));
-dhoni.addEventListener('click', display(dhoniimg));
-yuvraj.addEventListener('click', display(yuvrajimg));
-suresh.addEventListener('click', display(sureshimg));
-harbhajan.addEventListener('click', display(harbhajanimg));
-zaheer.addEventListener('click', display(zaheerimg));
-munaf.addEventListener('click', display(munafimg));
-sreesanth.addEventListener('click', display(sreesanthimg));
+document.body.addEventListener("mousedown",hideimage)
+// Sachin Tendulkar
+
+function showimage1(){
+  sachinimg.style.display='block';
+}
+function hideimage1() {
+  sachinimg.style.display='none';
+}
+sachin.addEventListener('click',showimage1)
+
+document.body.addEventListener("mousedown",hideimage1)
+// Gautam Gambhir
+function showimage2(){
+  gautamimg.style.display='block';
+}
+function hideimage2() {
+  gautamimg.style.display='none';
+}
+gautam.addEventListener('click',showimage2)
+
+document.body.addEventListener("mousedown",hideimage2)
+// Virat Kohli
+function showimage3(){
+  viratimg.style.display='block';
+}
+function hideimage3() {
+  viratimg.style.display='none';
+}
+virat.addEventListener('click',showimage3)
+
+document.body.addEventListener("mousedown",hideimage3)
+// MS Dhoni
+function showimage4(){
+  dhoniimg.style.display='block';
+}
+function hideimage4() {
+  dhoniimg.style.display='none';
+}
+dhoni.addEventListener('click',showimage4)
+
+document.body.addEventListener("mousedown",hideimage4)
+// Yuvraj Singh
+function showimage5(){
+  yuvrajimg.style.display='block';
+}
+function hideimage5() {
+  yuvrajimg.style.display='none';
+}
+yuvraj.addEventListener('click',showimage5)
+
+document.body.addEventListener("mousedown",hideimage5)
+// Suresh Raina
+function showimage6(){
+  sureshimg.style.display='block';
+}
+function hideimage6() {
+  sureshimg.style.display='none';
+}
+suresh.addEventListener('click',showimage6)
+
+document.body.addEventListener("mousedown",hideimage6)
+// Harbhajan Singh
+function showimage7(){
+  harbhajanimg.style.display='block';
+}
+function hideimage7() {
+  harbhajanimg.style.display='none';
+}
+harbhajan.addEventListener('click',showimage7)
+
+document.body.addEventListener("mousedown",hideimage7)
+// Zaheer Khan
+function showimage8(){
+  zaheerimg.style.display='block';
+}
+function hideimage8() {
+  zaheerimg.style.display='none';
+}
+zaheer.addEventListener('click',showimage8)
+
+document.body.addEventListener("mousedown",hideimage8)
+// Munaf Patel
+function showimage9(){
+  munafimg.style.display='block';
+}
+function hideimage9() {
+  munafimg.style.display='none';
+}
+munaf.addEventListener('click',showimage9)
+
+document.body.addEventListener("mousedown",hideimage9)
+
+// S Sreesanth
+function showimage10(){
+  sreesanthimg.style.display='block';
+}
+function hideimage10() {
+  sreesanthimg.style.display='none';
+}
+sreesanth.addEventListener('click',showimage10)
+
+document.body.addEventListener("mousedown",hideimage10)
